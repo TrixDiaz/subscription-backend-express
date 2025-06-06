7df28566b7c630c6329fa86c4942aff9dbbf5591
XirTech <john.darlucio022@gmail.com>
2025-06-07 00:43:49 +0800

    POST and GET users

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,47 @@
+import jwt from "jsonwebtoken";
+import {JWT_SECRET} from "../config/env.js";
+import User from "../models/user.model.js";
+
+const authrorize = async (req, res, next) => {
+  try {
+    let token;
+
+    // Check if the token is provided in the Authorization header
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    // If the token is not provided, return an unauthorized error
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized access, no token provided",
+      });
+    }
+
+    // Verify the token
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized access, user not found",
+      });
+    }
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Unauthorized access",
+      error: error.message,
+    });
+  }
+};
+
+export default authrorize;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,16 +1,14 @@
 import {Router} from "express";
+import {getUser, getUsers} from "../controllers/user.controller.js";
+import authrorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 // Fetch all users
-userRouter.get("/", (req, res) => {
-  res.send("Get all Users");
-});
+userRouter.get("/", getUsers);
 
 // Fetch a single user by ID
-userRouter.get("/:id", (req, res) => {
-  res.send("Get single User with ID: " + req.params.id);
-});
+userRouter.get("/:id", authrorize, getUser);
 
 // Create a new user
 userRouter.post("/", (req, res) => {
